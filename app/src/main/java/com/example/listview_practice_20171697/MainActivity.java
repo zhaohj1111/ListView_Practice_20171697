1de739f96647789e4164c1ae8b16e48dbd2e1ff8
zhaohj1111 <953200709@qq.com>
2020-03-17 15:23:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/listview_practice_20171697/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.listview_practice_20171697;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] names=new String[]{
+            "20161707",
+            "20161713",
+            "20171591",
+            "20171592",
+            "20171616",
+            "20171627",
+            "20171641",
+            "20171649",
+            "20171650",
+            "20171653",
+            "20171654",
+            "20171655",
+            "20171656",
+            "20171659",
+            "20171664",
+            "20171666",
+            "20171667",
+            "20171668",
+            "20171669",
+            "20171670",
+            "20171679",
+            "20171688",
+            "20171697",
+            "20171705",
+            "20171707",
+            "20171714",
+            "20171717",
+            "20171731",
+            "20171742",
+            "20175064",
+            "20175980",
+            "20175990",
+           };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,R.layout.item,R.id.item_text,names);
+        ListView listView=findViewById(R.id.list_view);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        //Toast.makeText(getApplicationContext(),"0",Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        //Toast.makeText(getApplicationContext(),"1",Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 12:
+                        break;
+                    case 13:
+                        break;
+                    case 14:
+                        break;
+                    case 15:
+                        break;
+                    case 16:
+                        break;
+                    case 17:
+                        break;
+                    case 18:
+                        break;
+                    case 19:
+                        break;
+                    case 20:
+                        break;
+                    case 21:
+                        break;
+                    case 22:
+                        break;
+                    case 23:
+                        break;
+
+                }
+            }
+        });
+
+    }
+}
