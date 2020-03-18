6fadd7901f2510911989a21c39eb5f46de625798
zhaohj1111 <953200709@qq.com>
2020-03-18 21:42:15 +0800

    final edit.

--- app/src/main/java/com/example/listview_practice_20171697/MainActivity.java
+++ app/src/main/java/com/example/listview_practice_20171697/MainActivity.java
@@ -5,10 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,30 +56,41 @@ public class MainActivity extends AppCompatActivity {
             "20175980",
             "20175990",
            };
-    //Intent intent0=new Intent(this,details0.class);
+    //Intent intent0=new Intent(this,details0.class)
 
+    private int [] imageId_bw=new int[]{R.drawable.bw1,R.drawable.bw2,R.drawable.bw3,R.drawable.bw4,R.drawable.bw5,
+            R.drawable.bw6,R.drawable.bw7,R.drawable.bw8,R.drawable.bw9,R.drawable.bw10,
+            R.drawable.bw11,R.drawable.bw12,R.drawable.bw13,R.drawable.bw14,R.drawable.bw15,
+            R.drawable.bw16,R.drawable.bw17,R.drawable.bw18,R.drawable.bw19,R.drawable.bw20,
+            R.drawable.bw21,R.drawable.bw22,R.drawable.bw23,R.drawable.bw24,R.drawable.bw25,
+            R.drawable.bw26,R.drawable.bw27,R.drawable.bw28,R.drawable.bw29,R.drawable.bw30,
+            R.drawable.bw31,R.drawable.bw32,R.drawable.bw33,R.drawable.bw34,R.drawable.bw35,
+            R.drawable.bw36,R.drawable.bw37,R.drawable.bw38,R.drawable.bw39,R.drawable.bw40};
+    private  ListView listView;
+    private MyAdapter myAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter arrayAdapter=new ArrayAdapter(this,R.layout.item,R.id.item_text,names);
-        ListView listView=findViewById(R.id.list_view);
-        listView.setAdapter(arrayAdapter);
+        listView=findViewById(R.id.list_view);
+
+        myAdapter=new MyAdapter();
+        listView.setAdapter(myAdapter);
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Intent intent0=new Intent();
-//                intent0.setClass(MainActivity.this,details0.class);
-//                startActivity(intent0);
+
                 for(int i=0;i<=names.length;i++){
 
                     if(i==position){
-                        String de="There are somedetails for "+i+" .";
+                        String de="There are somedetails for "+names[i]+" .";
                         Intent intent0=new Intent();
                         intent0.setClass(MainActivity.this,details0.class);
                         intent0.putExtra("de",de);
+                        intent0.putExtra("image",i);
                         startActivity(intent0);
 
                     }
@@ -77,5 +98,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return names.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view=getLayoutInflater().inflate(R.layout.item,null);
+
+            ImageView imageView=view.findViewById(R.id.item_Image);
+            TextView textView=view.findViewById(R.id.item_text);
+            imageView.setImageResource(imageId_bw[position]);
+            textView.setText(names[position]);
+
+            return view;
+        }
     }
 }
--- app/src/main/java/com/example/listview_practice_20171697/details0.java
+++ app/src/main/java/com/example/listview_practice_20171697/details0.java
@@ -4,21 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class details0 extends AppCompatActivity {
 
     TextView textView1;
+    ImageView imageView1;
 
+    private int [] imageId_color=new int[]{R.drawable.face1,R.drawable.face2,R.drawable.face3,R.drawable.face4,R.drawable.face5,
+            R.drawable.face6,R.drawable.face7,R.drawable.face8,R.drawable.face9,R.drawable.face10,
+            R.drawable.face11,R.drawable.face12,R.drawable.face13,R.drawable.face14,R.drawable.face15,
+            R.drawable.face16,R.drawable.face17,R.drawable.face18,R.drawable.face19,R.drawable.face20,
+            R.drawable.face21,R.drawable.face22,R.drawable.face23,R.drawable.face24,R.drawable.face25,
+            R.drawable.face26,R.drawable.face27,R.drawable.face28,R.drawable.face29,R.drawable.face30,
+            R.drawable.face31,R.drawable.face32,R.drawable.face33,R.drawable.face34,R.drawable.face35,
+            R.drawable.face36,R.drawable.face37,R.drawable.face38,R.drawable.face39,R.drawable.face40};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details0);
         textView1=findViewById(R.id.TXT0);
+        imageView1=findViewById(R.id.IM0);
         Intent intent = getIntent();
         String txt;
         txt=intent.getStringExtra("de");
         textView1.setText(txt);
+
+        int colorImage=intent.getIntExtra("image",0);
+        imageView1.setImageResource(imageId_color[colorImage]);
+
 
     }
 }
